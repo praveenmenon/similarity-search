97368cfc0c7f9e58aa14751485f9f6eee2b3d5ce
praveenmenon <pmenon@qwinixtech.com>
2018-12-20 18:00:31 -0500

    [master]first commit

---
+++ create_measurements_similarities.py
@@ -0,0 +1,47 @@
+# Author : Pruthvik Narayan Narayanaswamy (pnarayan@gmu.edu)
+# Script to index persistance vectors and write index to file
+# Using FAISS (see https://github.com/facebookresearch/faiss for official documentation and source code)
+
+import numpy as np
+import mysql.connector
+import faiss
+from faiss import normalize_L2
+
+def main():
+  # Set the database parameters to connect
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="password",
+    database="nmdbdev"
+    )
+
+  # Fetach all persistance vectors from DB 
+  # ordering by neuron_id important as we receive only id of the array from faiss, not the neuron_id iteslf.
+  mycursor = mydb.cursor()
+  mycursor.execute("SELECT * FROM measurements order by neuron_id")
+  myresult = mycursor.fetchall()
+
+  vectors = []
+  for x in myresult:
+    # Slice array to store only co-efficients
+    v = x[2:]
+    vectors.append(v)
+
+  # Convert co-efficients to float32
+  vectors = np.asarray(vectors).astype('float32')
+
+  # Set dimension of vector
+  d = 21
+  # Build the index
+  index = faiss.IndexFlatIP(d)   
+  print("Index trained : " + str(index.is_trained))
+  # Normalize vectors for indexing (faiss cosine similarity requires normalized vectors)
+  normalize_L2(vectors)
+  # Add vectors to the index
+  index.add(vectors)                  
+  print("Number of vectors indexed : " + str(index.ntotal))
+  # Write the index to file
+  faiss.write_index(index, "nmo_measurements.index")
+
+main()      
---
+++ flask_similarity.py
@@ -0,0 +1,91 @@
+import mysql.connector
+import numpy as np
+import faiss
+from flask import Flask
+from faiss import normalize_L2
+from flask import jsonify
+app = Flask(__name__)
+
+# Set the database parameters to connect
+mydb = mysql.connector.connect(
+	host="localhost",
+	user="root",
+	passwd="password",
+	database="nmdbdev"
+	)
+
+neuron_ids = {}
+measurements_neuron_ids = {}
+search_index = faiss.read_index("nmo_pvecs.index")
+measurements_search_index = faiss.read_index("nmo_measurements.index")
+
+@app.before_first_request
+def init_neuron_ids():
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT neuron_id FROM persistance_vector order by neuron_id")
+	myresult = mycursor.fetchall()
+
+	mycursor.execute("SELECT neuron_id FROM measurements order by neuron_id")
+	measurementsResults = mycursor.fetchall()
+
+	for index,neuron_id in enumerate(measurementsResults):
+		measurements_neuron_ids[index] = neuron_id[0]
+
+	for index,neuron_id in enumerate(myresult):
+		neuron_ids[index] = neuron_id[0]
+
+@app.route('/', methods=['GET'])
+def get():
+	return "similarity search main route"
+
+@app.route('/similarNeurons/measurements/<int:neuron_id>/<int:num_of_neurons>', methods=['GET'])
+def get_similar_neurons_measurements(neuron_id,num_of_neurons):
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT * FROM measurements where neuron_id = " + str(neuron_id))
+	myresult = mycursor.fetchall()
+	query_vector = []
+	for x in myresult:
+		query_vector.append(x[2:])	
+	result = {}
+	s = {}
+
+	# Normalize the query vector before searching (for cosine inner product search)
+	query_vector = np.asarray(query_vector).astype('float32')
+	normalize_L2(query_vector)
+	
+	# Actual Search
+	D, I = measurements_search_index.search(query_vector, num_of_neurons+1)
+
+	for index,val in enumerate(I[0][1:]):
+		s[neuron_ids[val]] = str(D[0][index+1])
+
+	result["similar_neuron_ids"] = s
+
+	return jsonify(result)
+
+@app.route('/similarNeurons/<int:neuron_id>/<int:num_of_neurons>', methods=['GET'])
+def get_similar_neurons(neuron_id,num_of_neurons):
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT * FROM persistance_vector where neuron_id = " + str(neuron_id))
+	myresult = mycursor.fetchall()
+	query_vector = []
+	for x in myresult:
+		query_vector.append(x[4:])	
+	result = {}
+	s = {}
+
+	# Normalize the query vector before searching (for cosine inner product search)
+	query_vector = np.asarray(query_vector).astype('float32')
+	normalize_L2(query_vector)
+	
+	# Actual Search
+	D, I = search_index.search(query_vector, num_of_neurons+1)
+
+	for index,val in enumerate(I[0][1:]):
+		s[neuron_ids[val]] = str(D[0][index+1])
+
+	result["similar_neuron_ids"] = s
+
+	return jsonify(result)
+
+
