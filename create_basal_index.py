b5f34a5d09d92a31724bdbe6f338a5b0044fb51b
praveenmenon <pmenon@qwinixtech.com>
2018-12-28 01:45:15 -0500

    [master]added four measurements similarity

---
+++ create_basal_index.py
@@ -0,0 +1,47 @@
+# Author : Pruthvik Narayan Narayanaswamy (pnarayan@gmu.edu)
+# Script to index persistance vectors and write index to file
+# Using FAISS (see https://github.com/facebookresearch/faiss for official documentation and source code)
+
+import numpy as np
+import mysql.connector
+import faiss
+from faiss import normalize_L2
+
+def main():
+  # Set the database parameters to connect
+  mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="password",
+    database="nmdbDev"
+    )
+
+  # Fetach all persistance vectors from DB 
+  # ordering by neuron_id important as we receive only id of the array from faiss, not the neuron_id iteslf.
+  mycursor = mydb.cursor()
+  mycursor.execute("SELECT neuron_id, Neuron_name, IFNULL(NULLIF(Soma_Surface, '' ), 0) as Soma_Surface, N_stems, N_bifs, N_branch, Width, Height, Depth, Diameter, Length, Surface, Volume, EucDistance, PathDistance, Branch_Order , Contraction, Fragmentation, Partition_asymmetry, Pk_classic, Bif_ampl_local, Bif_ampl_remote, Fractal_Dim FROM measurementsBS order by neuron_id")
+  myresult = mycursor.fetchall()
+
+  vectors = []
+  for x in myresult:
+    # Slice array to store only co-efficients
+    v = x[2:]
+    vectors.append(v)
+
+  # Convert co-efficients to float32
+  vectors = np.asarray(vectors).astype('float32')
+
+  # Set dimension of vector
+  d = 21
+  # Build the index
+  index = faiss.IndexFlatIP(d)   
+  print("Index trained : " + str(index.is_trained))
+  # Normalize vectors for indexing (faiss cosine similarity requires normalized vectors)
+  normalize_L2(vectors)
+  # Add vectors to the index
+  index.add(vectors)                  
+  print("Number of vectors indexed : " + str(index.ntotal))
+  # Write the index to file
+  faiss.write_index(index, "nmo_measurements_basal.index")
+
+main()      
--- flask_similarity.py
+++ flask_similarity.py
@@ -15,6 +15,11 @@ measurements_neuron_ids = {}
 search_index = faiss.read_index("nmo_pvecs.index")
 measurements_search_index = faiss.read_index("nmo_measurements.index")
 pvec_measurements_search_index = faiss.read_index("nmo_pvec_measurements.index")
+apical_measurements_search_index = faiss.read_index("nmo_measurements_apical.index")
+axon_measurements_search_index = faiss.read_index("nmo_measurements_axon.index")
+basal_measurements_search_index = faiss.read_index("nmo_measurements_basal.index")
+neurites_measurements_search_index = faiss.read_index("nmo_measurements_neurites.index")
+processes_measurements_search_index = faiss.read_index("nmo_measurements_processes.index")
 
 @app.before_first_request
 def init_neuron_ids():
@@ -59,6 +64,146 @@ def get_similar_neurons_measurements(neuron_id,num_of_neurons):
 	
 	# Actual Search
 	D, I = measurements_search_index.search(query_vector, num_of_neurons+1)
+
+	for index,val in enumerate(I[0][1:]):
+		s[neuron_ids[val]] = str(D[0][index+1])
+
+	result["similar_neuron_ids"] = s
+
+	return jsonify(result)
+
+@app.route('/similarNeurons/apical/<int:neuron_id>/<int:num_of_neurons>', methods=['GET'])
+def get_similar_neurons_measurements_apical(neuron_id,num_of_neurons):
+	mydb = mysql.connector.connect(host="localhost",user="root",passwd="password",database="nmdbDev")
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT neuron_id, Neuron_name, IFNULL(NULLIF(Soma_Surface, '' ), 0) as Soma_Surface, N_stems, N_bifs, N_branch, Width, Height, Depth, Diameter, Length, Surface, Volume, EucDistance, PathDistance, Branch_Order , Contraction, Fragmentation, Partition_asymmetry, Pk_classic, Bif_ampl_local, Bif_ampl_remote, Fractal_Dim FROM measurementsAP where neuron_id = " + str(neuron_id))
+	myresult = mycursor.fetchall()
+	query_vector = []
+	for x in myresult:
+		query_vector.append(x[2:])	
+	result = {}
+	s = {}
+	mycursor.close()
+	mydb.close()
+
+	# Normalize the query vector before searching (for cosine inner product search)
+	query_vector = np.asarray(query_vector).astype('float32')
+	normalize_L2(query_vector)
+	
+	# Actual Search
+	D, I = apical_measurements_search_index.search(query_vector, num_of_neurons+1)
+
+	for index,val in enumerate(I[0][1:]):
+		s[neuron_ids[val]] = str(D[0][index+1])
+
+	result["similar_neuron_ids"] = s
+
+	return jsonify(result)
+
+@app.route('/similarNeurons/axon/<int:neuron_id>/<int:num_of_neurons>', methods=['GET'])
+def get_similar_neurons_measurements_axon(neuron_id,num_of_neurons):
+	mydb = mysql.connector.connect(host="localhost",user="root",passwd="password",database="nmdbDev")
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT neuron_id, Neuron_name, IFNULL(NULLIF(Soma_Surface, '' ), 0) as Soma_Surface, N_stems, N_bifs, N_branch, Width, Height, Depth, Diameter, Length, Surface, Volume, EucDistance, PathDistance, Branch_Order , Contraction, Fragmentation, Partition_asymmetry, Pk_classic, Bif_ampl_local, Bif_ampl_remote, Fractal_Dim FROM measurementsAX where neuron_id = " + str(neuron_id))
+	myresult = mycursor.fetchall()
+	query_vector = []
+	for x in myresult:
+		query_vector.append(x[2:])	
+	result = {}
+	s = {}
+	mycursor.close()
+	mydb.close()
+
+	# Normalize the query vector before searching (for cosine inner product search)
+	query_vector = np.asarray(query_vector).astype('float32')
+	normalize_L2(query_vector)
+	
+	# Actual Search
+	D, I = axon_measurements_search_index.search(query_vector, num_of_neurons+1)
+
+	for index,val in enumerate(I[0][1:]):
+		s[neuron_ids[val]] = str(D[0][index+1])
+
+	result["similar_neuron_ids"] = s
+
+	return jsonify(result)
+
+@app.route('/similarNeurons/basal/<int:neuron_id>/<int:num_of_neurons>', methods=['GET'])
+def get_similar_neurons_measurements_basal(neuron_id,num_of_neurons):
+	mydb = mysql.connector.connect(host="localhost",user="root",passwd="password",database="nmdbDev")
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT neuron_id, Neuron_name, IFNULL(NULLIF(Soma_Surface, '' ), 0) as Soma_Surface, N_stems, N_bifs, N_branch, Width, Height, Depth, Diameter, Length, Surface, Volume, EucDistance, PathDistance, Branch_Order , Contraction, Fragmentation, Partition_asymmetry, Pk_classic, Bif_ampl_local, Bif_ampl_remote, Fractal_Dim FROM measurementsBS where neuron_id = " + str(neuron_id))
+	myresult = mycursor.fetchall()
+	query_vector = []
+	for x in myresult:
+		query_vector.append(x[2:])	
+	result = {}
+	s = {}
+	mycursor.close()
+	mydb.close()
+
+	# Normalize the query vector before searching (for cosine inner product search)
+	query_vector = np.asarray(query_vector).astype('float32')
+	normalize_L2(query_vector)
+	
+	# Actual Search
+	D, I = basal_measurements_search_index.search(query_vector, num_of_neurons+1)
+
+	for index,val in enumerate(I[0][1:]):
+		s[neuron_ids[val]] = str(D[0][index+1])
+
+	result["similar_neuron_ids"] = s
+
+	return jsonify(result)
+
+@app.route('/similarNeurons/neurites/<int:neuron_id>/<int:num_of_neurons>', methods=['GET'])
+def get_similar_neurons_measurements_neurites(neuron_id,num_of_neurons):
+	mydb = mysql.connector.connect(host="localhost",user="root",passwd="password",database="nmdbDev")
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT neuron_id, Neuron_name, IFNULL(NULLIF(Soma_Surface, '' ), 0) as Soma_Surface, N_stems, N_bifs, N_branch, Width, Height, Depth, Diameter, Length, Surface, Volume, EucDistance, PathDistance, Branch_Order , Contraction, Fragmentation, Partition_asymmetry, Pk_classic, Bif_ampl_local, Bif_ampl_remote, Fractal_Dim FROM measurementsNEU where neuron_id = " + str(neuron_id))
+	myresult = mycursor.fetchall()
+	query_vector = []
+	for x in myresult:
+		query_vector.append(x[2:])	
+	result = {}
+	s = {}
+	mycursor.close()
+	mydb.close()
+
+	# Normalize the query vector before searching (for cosine inner product search)
+	query_vector = np.asarray(query_vector).astype('float32')
+	normalize_L2(query_vector)
+	
+	# Actual Search
+	D, I = neurites_measurements_search_index.search(query_vector, num_of_neurons+1)
+
+	for index,val in enumerate(I[0][1:]):
+		s[neuron_ids[val]] = str(D[0][index+1])
+
+	result["similar_neuron_ids"] = s
+
+	return jsonify(result)
+
+@app.route('/similarNeurons/processes/<int:neuron_id>/<int:num_of_neurons>', methods=['GET'])
+def get_similar_neurons_measurements_processes(neuron_id,num_of_neurons):
+	mydb = mysql.connector.connect(host="localhost",user="root",passwd="password",database="nmdbDev")
+	mycursor = mydb.cursor()
+	mycursor.execute("SELECT neuron_id, Neuron_name, IFNULL(NULLIF(Soma_Surface, '' ), 0) as Soma_Surface, N_stems, N_bifs, N_branch, Width, Height, Depth, Diameter, Length, Surface, Volume, EucDistance, PathDistance, Branch_Order , Contraction, Fragmentation, Partition_asymmetry, Pk_classic, Bif_ampl_local, Bif_ampl_remote, Fractal_Dim FROM measurementsPR where neuron_id = " + str(neuron_id))
+	myresult = mycursor.fetchall()
+	query_vector = []
+	for x in myresult:
+		query_vector.append(x[2:])	
+	result = {}
+	s = {}
+	mycursor.close()
+	mydb.close()
+
+	# Normalize the query vector before searching (for cosine inner product search)
+	query_vector = np.asarray(query_vector).astype('float32')
+	normalize_L2(query_vector)
+	
+	# Actual Search
+	D, I = processes_measurements_search_index.search(query_vector, num_of_neurons+1)
 
 	for index,val in enumerate(I[0][1:]):
 		s[neuron_ids[val]] = str(D[0][index+1])
